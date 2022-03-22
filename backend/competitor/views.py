f52f48b55c1ec756f2191289db586ff616ca0b0d
RenzoTsai <runze.cai@u.nus.edu>
2022-03-22 20:55:38 +0800

    Update codes for changing team name and combining teams

--- backend/competitor/views.py
+++ backend/competitor/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import viewsets, status
+from django.core import serializers
 from rest_framework.decorators import api_view, action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from competitor.models import Competitor
-from competitor.serializer import CompetitorSerializer
+from competitor.models import Competitor, Team
+from competitor.serializer import CompetitorSerializer, TeamSerializer
 
 
 @api_view(['POST'])
@@ -33,10 +34,28 @@ class CompetitorViewset(viewsets.ModelViewSet):
         return super(CompetitorViewset, self).retrieve(request, *args, **kwargs)
 
     @action(detail=True, methods=['PUT'])
-    def team(self, request, *args, **kwargs):
+    def team(self, request, pk=None):
         # change team detail
+        try:
+            competitor = Competitor.objects.get(pk=pk)
+            current_team = competitor.team
+            current_team_uuid = competitor.team.uuid
+            target_team_uuid = request.data['team_uuid']
+            target_team = Team.objects.get(uuid=target_team_uuid)
+            if current_team_uuid == target_team_uuid and str(current_team.name) != str(request.data['team_name']):
+                Team.objects.filter(uuid=target_team_uuid).update(name=request.data['team_name'])
+                print("[Update Team Name] ", target_team.name, " -> ", request.data['team_name'])
+            elif current_team_uuid != target_team_uuid and target_team.name == request.data['team_name']:
+                competitor.team = target_team
+                competitor.save()
+                current_team.delete()
+                print("[Combine Teams] ", current_team.name, " -> ", target_team.name)
+            else:
+                return Response(status=status.HTTP_400_BAD_REQUEST, data={"Please check the uuid or team name"})
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={"Please check the uuid or team name"})
 
-        return Response(status=status.HTTP_200_OK, data={})
+        return Response(status=status.HTTP_200_OK, data=serializers.serialize('json', [target_team, ]))
 
     @action(detail=True, methods=['POST', 'GET'])
     def task(self, request, *args, **kwargs):
