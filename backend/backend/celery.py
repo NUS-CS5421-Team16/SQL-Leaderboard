7f1c2872dfa76a00554b97024c8806003cc1365b
RenzoTsai <runze.cai@u.nus.edu>
2022-03-29 17:59:38 +0800

    Update celery.py

--- backend/backend/celery.py
+++ backend/backend/celery.py
@@ -3,7 +3,7 @@ import os
 from celery import Celery
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings2')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 app = Celery("sqltaskrunner")
 
 app.conf.broker_url = 'redis://localhost:6379/0'
