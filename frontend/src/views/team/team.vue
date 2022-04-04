59baf61b45cd5d8681e779bdf2f0f190dddee678
vanhwz <van136682262@gmail.com>
2022-04-05 00:14:29 +0800

    display teammates&&modify team view

--- frontend/src/views/team/team.vue
+++ frontend/src/views/team/team.vue
@@ -1,17 +1,44 @@
 <template>
     <div style="padding: 20px;">
-        <el-button style="margin-bottom: 20px;" type="primary" @click="getDetail">Refresh</el-button>
+        <el-container>
+            <el-button style="margin-bottom: 20px;" type="primary" @click="getDetail">Refresh</el-button>
+        </el-container>
+        <el-row>
+            <h1>
+                Teammates Info
+            </h1>
+            <el-table
+                :data="teammateTableData"
+                stripe
+                style="width: 100%">
+                <el-table-column prop="competitor id" label="competitor id"></el-table-column>
+                <el-table-column prop="name" label="name"></el-table-column>
+            </el-table>
+        </el-row>
+        <el-row>
+            <h1>Change Team Info</h1>
+        </el-row>
+        <el-row>
+            <div>Two Usages:</div>
+        </el-row>
+        <el-row>
+            <div>1. Change your Team: input your target Team Name and Team UUID, then click "update".</div>
+        </el-row>
+        <el-row>
+            <div>2. Change your Team Name: only input new Team Name and leave UUID unchanged, then click "update".</div>
+        </el-row>
         <el-form :model="form" label-width="120px">
             <el-form-item label="Team Name">
-                <el-input v-model="form.name" />
+                <el-input v-model="form.team_name" />
             </el-form-item>
             <el-form-item label="Team ID">
-                <el-input v-model="form.id" />
+                <el-input v-model="form.team_uuid" />
             </el-form-item>
             <el-form-item>
                 <el-button type="primary" @click="onSubmit">Update</el-button>
             </el-form-item>
         </el-form>
+
     </div>
 </template>
 
@@ -26,9 +53,11 @@ const state = reactive({
     cid: computed(() => store.getters.getCid),
 })
 
+const teammateTableData = ref([]);
+
 const form = reactive({
-    name: '',
-    id: '',
+    team_name: '',
+    team_uuid: '',
 })
 
 const onSubmit = async () => {
@@ -40,9 +69,12 @@ const onSubmit = async () => {
 }
 
 const getDetail = async () => {
-    const res = await getCompetitor(state.cid);
-    form.name = (res as any).name;
-    form.id = (res as any).id;
+    const res = await getCompetitor(sessionStorage.getItem('cid'));
+    form.team_name = (res as any).team_name;
+    form.team_uuid = (res as any).team_uuid;
+
+    console.log(res)
+    teammateTableData.value = (res as any).teammates;
 }
 
 onMounted(() => {
@@ -51,4 +83,11 @@ onMounted(() => {
 })
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .el-row {
+    margin-bottom: 20px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+</style>
