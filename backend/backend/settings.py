c57fc7a48d57ebb00e303a41f205e20eb63939d0
vanhwz <van136682262@gmail.com>
2022-04-07 23:59:22 +0800

    backend remove basic authentication

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -55,7 +55,6 @@ MIDDLEWARE = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ]
 }
